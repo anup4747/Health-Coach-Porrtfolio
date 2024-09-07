a9dca5ad0307243e7b75cd725c487525deae5def
Anup <119606225+anup4747@users.noreply.github.com>
2024-09-07 10:35:42 +0530

    Testimonies image update
    Testimonies image update

--- src/Components/Testimonies.tsx
+++ src/Components/Testimonies.tsx
@@ -1,3 +1,5 @@
+import Testimonialimg1 from '../assets/image.jpg'
+
 export default function Testimonies() {
 
     return (
@@ -9,31 +11,31 @@ export default function Testimonies() {
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 1" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 1" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 2" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 2" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 3" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 3" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 4" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 4" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 5" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 5" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 6" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 6" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 7" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 7" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 8" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 8" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
           <div className="w-full h-full">
-            <img src="../assets/image.jpg" alt="Gallery Image 9" className="object-cover w-full h-full rounded-lg shadow-lg"/>
+            <img src={Testimonialimg1} alt="Gallery Image 9" className="object-cover w-full h-full rounded-lg shadow-lg"/>
           </div>
         </div>
       </div>
